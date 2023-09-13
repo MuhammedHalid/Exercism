4655b7f1a2ca2f6c34c65eb84501a2a8c42d4f81
MuhammedHalid <91598377+MuhammedHalid@users.noreply.github.com>
2023-09-13 22:42:05 +0530

    armstrong

--- dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,31 +1,12 @@
-import 'dart:math';
-
-// void main() {
-//   final a = ArmstrongNumbers();
-//   a.isArmstrongNumber('371');
-// }
-
 class ArmstrongNumbers {
   // Put your code here
-  bool isArmstrongNumber(String nums) {
-    num rem = 0;
-   // BigInt a = BigInt.from(0);
-    //BigInt sum = BigInt.parse('0',radix: 36);
-    double sum = 0;
-    print(sum.runtimeType);
-    num Number = int.parse(nums);
-
-    while (Number > 0) {
-      rem = Number % 10;
-      num power = pow(rem, nums.length);
-      sum = sum + power;
-      //sum = sum + BigInt.from(power);
-      Number = (Number / 10).floor();
+  bool isArmstrongNumber(String input) {
+    final digits = input.split('').map<BigInt>(BigInt.parse).toList();
+    print(digits);
+    var sum = BigInt.from(0);
+    for (var digit in digits) {
+      sum += digit.pow(digits.length);
     }
-    String a = sum.toString();
-    if (a == nums) {
-      return true;
-    } else
-      return false;
+    return sum == BigInt.parse(input);
   }
 }
